0f03e988dc2f4d876feca50324dda7074303f6a9
YOU SHIH WEI <sengosha@gmail.com>
2020-12-29 19:19:26 +0900

    move footer to app

--- pages/_app.js
+++ pages/_app.js
@@ -9,25 +9,33 @@ import Hero from '@/components/shared/Hero'
 
 const MyApp = ( {Component, pageProps}) => {
 
+  const isHomePage = () => Component.name === 'Home'
+
   return (
     <div className="portfolio-app">
       <Navbar />
-      {pageProps.appData}
-      { Component.name === 'Home' && <Hero /> }
+      { isHomePage() && <Hero /> }
       <div className="container">
         <Component {...pageProps} />
       </div>
+      { isHomePage() &&
+        <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+          <div className="container text-center">
+            <small>Copyright &copy; Your Website</small>
+          </div>
+        </footer>
+      }
     </div>
   )
 }
 
-MyApp.getInitialProps = async (context) => {
-  //
-  // App is the page about to be render
-  // check the page component have getInitialProps
-  const initialProps = App.getInitialProps && await App.getInitialProps(context);
+// MyApp.getInitialProps = async (context) => {
 
-  // initialProps.pageProps is the component`s data from its getInitialProps
-  return { pageProps : {appData: 'Hello _App Component', ...initialProps.pageProps}}
-}
+//   // App is the page about to be render
+//   // check the page component have getInitialProps
+//   const initialProps = App.getInitialProps && await App.getInitialProps(context);
+
+//   // initialProps.pageProps is the component`s data from its getInitialProps
+//   return { pageProps : {appData: 'Hello _App Component', ...initialProps.pageProps}}
+// }
 export default MyApp;
