0a395710a9726c3658fdcbff86927f36d37c1707
YOU SHIH WEI <sengosha@gmail.com>
2020-12-29 23:34:30 +0900

    add AppLink component

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,33 +1,41 @@
 import { Navbar, Nav } from 'react-bootstrap';
 import Link from 'next/link';
 
+
+const AppLink = ({children, className, href}) =>
+  <Link href={href}>
+    <a className={className}>{children}</a>
+  </Link>
+
 const AppNavbar = () => {
   return (
     <div className="navbar-wrapper">
       <Navbar expand="lg" className="navbar-dark fj-mw9">
-        <Navbar.Brand className="mr-3 font-weight-bold" href="#">YOU SHIH WEI</Navbar.Brand>
+        <AppLink
+          href="/"
+          className="navbar-brand mr-3 font-weight-bold">
+          YOU SHIH WEI
+        </AppLink>
         <Navbar.Toggle></Navbar.Toggle>
         <Navbar.Collapse>
           <Nav className="mr-auto">
-            <Link href="/portfolios">
-              <a className="nav-link mr-3">
+            <AppLink href="/portfolios" className="nav-link mr-3">
                 Portfolios
-              </a>
-            </Link>
-            <Nav.Link href="/forum/categories" className="mr-3">
+            </AppLink>
+            <AppLink href="/forum/categories" className="nav-link mr-3">
               Forum
-            </Nav.Link>
-            <Nav.Link href="#" className="mr-3">
+            </AppLink>
+            <AppLink href="/cv" className="nav-link mr-3">
               Cv
-            </Nav.Link>
+            </AppLink>
           </Nav>
           <Nav>
-            <Nav.Link href="/login" className="mr-3">
+            <AppLink href="/login" className="mr-3">
               Sign In
-            </Nav.Link>
-            <Nav.Link href="#" className="mr-3 btn btn-success bg-green-2 bright">
+            </AppLink>
+            <AppLink href="/register" className="mr-3 btn btn-success bg-green-2 bright">
               Sign Up
-            </Nav.Link>
+            </AppLink>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
